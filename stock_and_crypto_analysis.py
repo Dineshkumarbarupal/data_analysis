06f259f56b16dd2d7da391473f8f1f6cb066de8b
vihaan barupal <159883311+Dineshkumarbarupal@users.noreply.github.com>
2025-01-06 14:50:39 +0530

    this is a commit

--- stock_and_crypto_analysis.py
+++ stock_and_crypto_analysis.py
@@ -1,14 +1,50 @@
 import requests
+import csv
 
 url = "https://tradingview-ta-api-technical-analysis.p.rapidapi.com/get_symbols_from_exchange"
-
-querystring = {"exchange":"NASDAQ"}
+querystring = {"exchange": "NASDAQ"}
 
 headers = {
-	"x-rapidapi-key": "9a623cdfcemsh9266ff7a3a527eap14f3d6jsnce6b2e1a1e5d",
-	"x-rapidapi-host": "tradingview-ta-api-technical-analysis.p.rapidapi.com"
+    "x-rapidapi-key": "9a623cdfcemsh9266ff7a3a527eap14f3d6jsnce6b2e1a1e5d",
+    "x-rapidapi-host": "tradingview-ta-api-technical-analysis.p.rapidapi.com"
 }
 
 response = requests.get(url, headers=headers, params=querystring)
 
-print(response.json())
+data = response.json()
+print(data)
+
+file_name = "nasdaq_symbols.text"
+
+# with open('output.txt', 'w') as file:   # Save data into text file
+#     for key, value in data.items():
+#         file.write(f'{key}: {value}\n')
+
+# with open('output.txt', 'w', newline='') as file:
+#     writer = csv.writer(file)
+#     for key, value in data.items():
+#         writer.writerow([key, value])
+
+if isinstance(data, dict) and "data" in data:
+    symbol_list = data["data"]
+elif isinstance(data, list):
+    symbol_list = data  
+else:
+    symbol_list = [] 
+
+if symbol_list:
+    file_name = "nasdaq_symbols.csv"
+
+    with open(file_name, mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Code", "Name"]) 
+
+        for item in symbol_list:
+            writer.writerow([item.get("code", ""), item.get("name", "")])
+
+    print(f"Data successfully written to {file_name}")
+else:
+    print("No data to write to CSV. Please check the API response.")
+
+
+
--- youtube_data_analysis.py
+++ youtube_data_analysis.py
@@ -4,6 +4,7 @@ from IPython.display import JSON
 from dateutil import parser
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 
 
@@ -14,28 +15,26 @@ api_service_name = "youtube"
 api_version = "v3"
 
 
-# Get credentials and create an API client
 youtube = build(
     api_service_name, api_version, developerKey=api_keys)
 
-# request = youtube.channels().list(
-#     part="snippet,contentDetails,statistics",
-#     id=",".join(channel_ids)
-# )
-# response = request.execute()
-# print(response)
+request = youtube.channels().list(
+    part="snippet,contentDetails,statistics",
+    id=",".join(channel_ids)
+)
+
+response = request.execute()
+print(response)
 
 def get_channel_stats(youtube, channel_ids):
     all_data = []
 
-    # Create request to get channel details
     request = youtube.channels().list(
         part="snippet,contentDetails,statistics",
         id=",".join(channel_ids)
     )
     response = request.execute()
 
-    # Loop through the items in the response
     for item in response['items']:
         data = {
             'channelName': item['snippet']['title'],
@@ -46,27 +45,24 @@ def get_channel_stats(youtube, channel_ids):
         }
         all_data.append(data)
 
-    # Return data as a Pandas DataFrame
     return pd.DataFrame(all_data)
-# print(response)
+
 
 channel_data = get_channel_stats(youtube,channel_ids)
 print(channel_data)
 
 
-playlist_id = "UUeVMnSShP_Iviwkknt83cww"  # Example playlist ID
+playlist_id = "UUeVMnSShP_Iviwkknt83cww" 
 def get_video_ids(youtube, playlist_id):
     video_ids = []
 
-    # Create request to get playlist items
     request = youtube.playlistItems().list(
         part="snippet,contentDetails",
         playlistId=playlist_id,
         maxResults=50
     )
     response = request.execute()
-
-    # Loop through the items in the response
+    
     for item in response['items']:
         video_ids.append(item['contentDetails']['videoId'])
 
@@ -125,9 +121,12 @@ videos_data = get_video_details(youtube,video_ids)
 
 # print(videos_data)
 
-
-ax = sns.barplot(x= 'title', y= 'viewCount',data= videos_data.sort_values('viewCount',ascending=False)[0:9])
+# ax = sns.barplot(x = 'title', y = 'viewCount', data= videos_data.sort_values('viewCount',ascending=False)[0:9])
 # plot = ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
-ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f} k'.format(x / 1000)))
+# ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000) + 'K'))
+
+sns.violinplot(videos_data['channelTitle'],videos_data['viewCount'])
 plt.show()
+
+
 
